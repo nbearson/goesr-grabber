5189b0b3dd74849778356dff7c4bb4cdc16bc3a8
Nick Bearson <nickb@ssec.wisc.edu>
2022-08-02 15:23:49 -0500

    switch to boto3 for aws support and add tqdm progress bars

--- goesr-grabber.py
+++ goesr-grabber.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python3
+
 import typer
 from datetime import datetime, timedelta
 from enum import Enum
-import s3fs
+from tqdm import tqdm
+import boto3
+from botocore import UNSIGNED
+from botocore.client import Config
 
 class GOESRSatellite(str, Enum):
     goes16 = "goes16"
@@ -28,29 +33,47 @@ PRODUCTS=['ABI-L1b-RadF',
           'SUVI-L1b-He303',]
 
 
+def hook(t):
+  def inner(bytes_amount):
+    t.update(bytes_amount)
+  return inner
+
+
 def main(satellite:GOESRSatellite=GOESRSatellite.goes16,
          start_time:datetime=datetime(year=2022, month=8, day=1, hour=18),
          end_time:datetime=datetime(year=2022, month=8, day=1, hour=19),
+         progress_bar:bool=True,
          dry_run:bool=True):
 
     if dry_run:
-        print(f"Listing files of {satellite} from {start_time} to {end_time}")
+        print(f"Listing {satellite} files from {start_time} to {end_time}")
     else:
-        print(f"Downloading files of {satellite} from {start_time} to {end_time}")
+        print(f"Downloading {satellite} files from {start_time} to {end_time}")
 
-    fs = s3fs.S3FileSystem(anon=True)
-    s3root = f's3://noaa-{satellite}'
-
+    s3 = boto3.client('s3', region_name='us-east-1', config=Config(signature_version=UNSIGNED))
+    bucket = f"noaa-{satellite}"
     for product in PRODUCTS:
         delta = end_time - start_time
         for hour in [start_time + timedelta(hours=i) for i in range((delta.days*24) + (delta.seconds // 3600))]:
-            remote = f'{s3root}/{product}/{hour.strftime("%Y/%j/%H")}/'
-            print(remote)
-            files = fs.ls(remote)
-            for file in files:
-                print(file)
-                if not dry_run:
-                    fs.get(file, file.split('/')[-1])
+            prefix = f'{product}/{hour.strftime("%Y/%j/%H")}/'
+            paginator = s3.get_paginator('list_objects_v2')
+            operation_parameters = {'Bucket': bucket,
+                                    'Prefix': prefix}
+            page_iterator = paginator.paginate(**operation_parameters)
+            for page in page_iterator:
+                for obj in page['Contents']:
+                    filesize = obj['Size']
+                    filename = obj['Key'].split('/')[-1]
+                    if dry_run:
+                        print(f"{filename}")
+                    else:
+                        if progress_bar:
+                            with tqdm(total=filesize, unit='B', unit_scale=True, desc=filename) as t:
+                                s3.download_file(bucket, obj['Key'], filename, Callback=hook(t))
+                        else:
+                            print(f"{filename}")
+                            s3.download_file(bucket, obj['Key'], filename)
+
 
 if __name__ == "__main__":
     typer.run(main)
